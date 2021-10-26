488aa12b9f634161646953ba5ffb0dde209461ce
Sukairo-02 <reka.serzh@yandex.ru>
2021-10-26 05:44:38 +0300

    Moved calculation code to separate file, deadline calculation now fully functional

--- src/routes/classes/pricedeadlineClass.ts
+++ src/routes/classes/pricedeadlineClass.ts
@@ -1,11 +1,6 @@
 import { RequestHandler } from 'express'
+import pdCalc from './utility/pdCalc'
 const config = require('config')
-
-type corDline = {
-	time: string
-	deadline: number
-	deadlineDate: Date
-}
 
 type reqType = {
 	language: string
@@ -13,188 +8,10 @@ type reqType = {
 	count: number
 }
 
-function getPrice(lang: string, mType: string, letCnt: number): number {
-	if (letCnt <= 0) {
-		throw 'Unacceptable amount of symbols!'
-	}
-
-	let pl: number, min: number
-	try {
-		;({ pl, min } = config.get(`calc.lang.${lang}`))
-	} catch (e) {
-		throw 'Unsupported language!'
-	}
-
-	const discTypes = config.get('calc.discTypes')
-	let typeMod: number = 1
-	if (!discTypes.find((a: string) => mType === a)) {
-		typeMod = config.get('calc.uTypesTax')
-	}
-	const price: number = pl * typeMod * letCnt
-	const result = price < min ? min : price
-	return result
-}
-
-enum intTime {
-	second = 1000,
-	minute = 60000,
-	hour = 3600000,
-	day = 86400000,
-}
-
-function getDeadline(lang: string, letCnt: number): corDline {
-	const oldTz = process.env.TZ
-	console.log(oldTz)
-	//changing timeZone to UTC
-	process.env.TZ = 'UTC'
-	console.log(process.env.TZ)
-	let lph: number
-	try {
-		;({ lph } = config.get(`calc.lang.${lang}`))
-	} catch (e) {
-		throw 'Unsupported language!'
-	}
-
-	//getting current time in set timezone
-	let deadline: Date = new Date()
-
-	//work schedule in days
-	const wDays: [number, number] = config.get('calc.workDays')
-	//determining order of work days (ex: monday to friday - false, friday to monday - true)
-	const altDays: boolean = wDays[1] < wDays[0]
-	//work schedule in hours
-	const wTime: [number, number] = config.get('calc.workTime')
-	//same for time (ex: 2:00 - 18:00 - false, 18:00 - 2:00 - true)
-	const altTime: boolean = wTime[1] < wTime[0]
-
-	//hours of work per day
-	const hpd = altTime ? 24 - wTime[0] + wTime[1] : wTime[1] - wTime[0]
-	//calculate completion time
-	const minTime = config.get('calc.minTime')
-	const estTime = (letCnt / lph) * intTime.hour + 30 * intTime.minute
-	let finalTime = estTime < minTime ? minTime : estTime
-	const resTime = Math.floor(finalTime)
-
-	let today = new Date(
-		new Date(
-			new Date(0).toDateString() + ', ' + deadline.toTimeString()
-		).toLocaleString('en-GB', { timeZone: config.get('calc.timeZone') })
-	).getTime()
-
-	console.log('Init deadline:', deadline)
-	console.log(today)
-
-	//Calculate deadline
-	//start with resetting current day to 00:00 for simpler calculation
-	//rewinded worktime will be added to completion time
-	if (altTime) {
-		if (
-			today < wTime[0] * intTime.hour &&
-			today >= wTime[1] * intTime.hour
-		) {
-			finalTime += wTime[1] * intTime.hour
-			console.log('FinalTimeCase1: ', finalTime)
-		} else if (today >= 0 && today < wTime[1] * intTime.hour) {
-			finalTime += today
-			console.log('FinalTimeCase2: ', finalTime)
-		} else {
-			console.log(finalTime)
-			finalTime +=
-				wTime[1] * intTime.hour + today - intTime.hour * wTime[0]
-			console.log('FinalTimeCase3: ', finalTime)
-		}
-	} else {
-		if (
-			today >= wTime[0] * intTime.hour &&
-			today < wTime[1] * intTime.hour
-		) {
-			finalTime += today - wTime[0] * intTime.hour
-		} else if (today >= wTime[1] * intTime.hour) {
-			finalTime += (wTime[1] - wTime[0]) * intTime.hour
-		}
-	}
-
-	console.log(altDays, altTime)
-
-	console.log('Final time: ', new Date(finalTime))
-	let fullDays = Math.floor(finalTime / (hpd * intTime.hour))
-	finalTime -= fullDays * hpd * intTime.hour
-	console.log('Full days: ', fullDays)
-	console.log('Final time: ', new Date(finalTime))
-	while (fullDays) {
-		let move = 0
-		const day = deadline.getDay()
-		//if today is in workdays, move to the end of work week,
-		//subtracting passed days from calculated days to finish processing
-		if (
-			altDays
-				? day >= wDays[0] || day <= wDays[1]
-				: day >= wDays[0] && day <= wDays[1]
-		) {
-			//determining how much days is left untill end of work week
-			const tillEnd =
-				1 +
-				(altDays
-					? day >= wDays[0]
-						? 6 - day + wDays[1]
-						: wDays[1] - day
-					: wDays[1] - day)
-			move = fullDays > tillEnd ? tillEnd : fullDays
-			fullDays -= move
-		} else {
-			//else move to the beginning of work week
-			move =
-				1 +
-				(altDays
-					? wDays[0] - day
-					: day < wDays[0]
-					? wDays[0] - day
-					: 6 - day + wDays[0])
-		}
-
-		deadline = new Date(deadline.getTime() + move * intTime.day)
-	}
-
-	console.log('Deadline after days:', deadline)
-
-	//if there's still time to fill, go to the beginning of work day and add remaining time
-	//but if there's none left, set deadline to be the end of last day's workday in case of altDays = false
-	//for altDays = true, calculation be considered done.
-	if (altDays) {
-		deadline = new Date(
-			deadline.getTime() +
-				(finalTime > wTime[1] * intTime.hour
-					? wTime[0] * intTime.hour +
-					  finalTime -
-					  wTime[0] * intTime.hour
-					: finalTime)
-		)
-	} else {
-		deadline = new Date(
-			deadline.getTime() +
-				(finalTime
-					? finalTime + wTime[0] * intTime.hour
-					: -1 * (intTime.day - wTime[1] * intTime.hour))
-		)
-	}
-	console.log('OPDLSJMG', new Date(finalTime))
-
-	//final bits for 'time' field in return
-	console.log(new Date(resTime))
-	const hours = Math.floor(resTime / intTime.hour)
-	const minutes = Math.floor((resTime % intTime.hour) / intTime.minute)
-	const seconds = Math.floor((resTime % intTime.minute) / intTime.second)
-	const millis = resTime % intTime.second
-
-	let result: corDline = {
-		time: '' + hours + ':' + minutes + ':' + seconds + '.' + millis,
-		deadline: Math.floor(deadline.getTime() / 1000),
-		deadlineDate: deadline,
-	}
-
-	//return to server's original timezone
-	process.env.TZ = oldTz
-	return result
+type corDline = {
+	time: string
+	deadline: number
+	deadlineDate: Date
 }
 
 class pdCalculator {
@@ -202,12 +19,13 @@ class pdCalculator {
 		try {
 			const reqData: reqType = (<any>req).body
 			const { language, mimetype, count } = reqData
-
 			let price: number, deadline: corDline
 			try {
-				price = getPrice(language, mimetype, count)
-				deadline = getDeadline(language, count)
+				let pdc = new pdCalc(language, mimetype, count)
+				price = pdc.getPrice()
+				deadline = pdc.getDeadline()
 			} catch (er) {
+				console.log(er)
 				return res.status(403).json({ er })
 			}
 
---
+++ src/routes/classes/utility/pdCalc.ts
@@ -0,0 +1,245 @@
+import moment from 'moment-timezone'
+const config = require('config')
+
+enum intTime {
+	second = 1000,
+	minute = 60000,
+	hour = 3600000,
+	day = 86400000,
+}
+
+type corDline = {
+	time: string
+	deadline: number
+	deadlineDate: Date
+}
+
+type lang = {
+	pl: number
+	min: number
+	lph: number
+}
+
+class pdCalc {
+	private wTime: [number, number]
+
+	private wDays: [number, number]
+
+	private altTime: boolean
+
+	private altDays: boolean
+
+	private lang: lang
+
+	private mType: string
+
+	private letCnt: number
+
+	private reqTime: Date
+
+	constructor(
+		lang: string,
+		mType: string,
+		letCnt: number,
+		wTime?: [number, number] | undefined,
+		wDays?: [number, number] | undefined,
+		reqTime?: Date | undefined
+	) {
+		//work days (0 - sunday, 6 - saturday)
+		this.wDays = wDays || config.get('calc.workDays')
+		//determining order of work days (ex: monday to friday - false, friday to monday - true)
+		this.altDays = this.wDays[1] < this.wDays[1]
+		//work time in hours
+		const tmpTime = wTime || config.get('calc.workTime')
+		this.wTime = [~~tmpTime[0], ~~tmpTime[1]]
+		//same for time (ex: 2:00 - 18:00 - false, 18:00 - 2:00 - true)
+		this.altTime = this.wTime[1] < this.wTime[0]
+		try {
+			this.lang = config.get(`calc.lang.${lang}`)
+		} catch {
+			throw 'Unsupported language!'
+		}
+		this.mType = mType
+		if (letCnt <= 0) {
+			throw 'Unacceptable amount of symbols!'
+		}
+		this.letCnt = letCnt
+		this.reqTime = reqTime || new Date()
+	}
+
+	//same to Date.getTime() but in set zone instead of UTC
+	private getTotalMillis(date: moment.Moment): number {
+		return (
+			date.valueOf() +
+			(intTime.minute * date.utcOffset() - +!date.isDST() * intTime.hour)
+		)
+	}
+
+	//get today's time (H/M/S/MS only) in ms
+	private getMillisToday(date: moment.Moment): number {
+		return (
+			date.hour() * intTime.hour +
+			date.minute() * intTime.minute +
+			date.second() * intTime.second +
+			date.millisecond()
+		)
+	}
+
+	private isWorkDay(date: moment.Moment): boolean {
+		const testable = moment(date)
+		if (
+			this.altTime &&
+			this.getMillisToday(date) < this.wTime[1] * intTime.hour
+		) {
+			testable.subtract(this.wTime[1], 'ms')
+		}
+		const day = testable.day()
+		return !(this.altDays
+			? day < this.wDays[0] && day > this.wDays[1]
+			: day > this.wDays[1] || day < this.wDays[0])
+	}
+
+	private isWorkTime(date: moment.Moment): boolean {
+		const millis = this.getMillisToday(date)
+		const mWorkTime: [number, number] = [
+			this.wTime[0] * intTime.hour,
+			this.wTime[1] * intTime.hour,
+		]
+		return !(this.altTime
+			? millis >= mWorkTime[1] && millis < mWorkTime[0]
+			: millis < mWorkTime[0] || millis >= mWorkTime[1])
+	}
+
+	//moves date to beginning of workdays
+	private moveToWDay(date: moment.Moment): boolean {
+		date.add(
+			this.wDays[0] - date.day() + 7 * +(date.day() >= this.wDays[0]),
+			'd'
+		)
+		return true
+	}
+
+	private moveToWTime(date: moment.Moment): boolean {
+		const today = this.getMillisToday(date)
+		date.add(
+			this.wTime[0] * intTime.hour -
+				today +
+				intTime.day * +(today > this.wTime[0] * intTime.hour),
+			'ms'
+		)
+		return true
+	}
+
+	//fill remaining full days
+	private fillWorkDays(date: moment.Moment, toFill: number) {
+		if (!this.isWorkDay(date)) {
+			this.moveToWDay(date)
+		}
+		const daysLeft = 1 + this.wDays[1] + 7 * +this.altDays - date.day()
+		if (daysLeft < toFill) {
+			this.tmp()
+			toFill -= daysLeft
+			date.add(daysLeft, 'd')
+			this.moveToWDay(date)
+		}
+		date.add(toFill, 'd')
+	}
+
+	private tmp() {
+		console.log('IWORK')
+		return true
+	}
+
+	//fill remaining time
+	private fillWorkTime(date: moment.Moment, toFill: number) {
+		if (!this.isWorkDay(date)) {
+			this.moveToWDay(date)
+		}
+
+		if (!this.isWorkTime(date)) {
+			this.moveToWTime(date)
+		}
+
+		const timeLeft =
+			this.wTime[1] * intTime.hour +
+			intTime.day * +this.altTime -
+			this.getMillisToday(date)
+
+		if (timeLeft < toFill) {
+			toFill -= timeLeft
+			date.add(timeLeft, 'ms')
+			this.moveToWTime(date)
+			if (!this.isWorkDay(date)) {
+				this.moveToWDay(date)
+			}
+		}
+
+		date.add(toFill, 'ms')
+	}
+
+	public getPrice(): number {
+		let { pl, min } = this.lang
+
+		const discTypes = config.get('calc.discTypes')
+		let typeMod: number = 1
+		if (!discTypes.find((a: string) => this.mType === a)) {
+			typeMod = config.get('calc.uTypesTax')
+		}
+
+		const price: number = Math.round(pl * typeMod * this.letCnt)
+		const result = price < min ? min : price
+		return result
+	}
+
+	public getDeadline(): corDline {
+		//remember server's timezone
+		const origTimezone = moment.tz.guess()
+		//change timezone to config's
+		moment.tz.setDefault(config.get('calc.timeZone'))
+
+		//begin calculation with setting current time
+		let dLine: moment.Moment = moment(this.reqTime)
+		//days of work per week
+		const dpw =
+			1 +
+			(this.altDays
+				? 7 - this.wDays[0] + this.wDays[0]
+				: this.wDays[1] - this.wDays[0])
+
+		//hours of work per day
+		const hpd = this.altTime
+			? 24 - this.wTime[0] + this.wTime[1]
+			: this.wTime[1] - this.wTime[0]
+
+		//calculate completion time
+		const minTime = config.get('calc.minTime')
+		const estTime =
+			(this.letCnt / this.lang.lph) * intTime.hour + 30 * intTime.minute
+		let finalTime = estTime < minTime ? minTime : estTime
+		const resTime = Math.floor(finalTime)
+		let fullDays = Math.floor(finalTime / (hpd * intTime.hour))
+		finalTime -= fullDays * hpd * intTime.hour
+		let fullWeeks = Math.floor(fullDays / dpw)
+		fullDays -= fullWeeks * dpw
+
+		//calculate deadline
+		this.fillWorkTime(dLine, finalTime)
+		this.fillWorkDays(dLine, fullDays)
+		dLine.add(fullWeeks, 'w')
+
+		//final bits for 'time' field in return
+		const hours = Math.floor(resTime / intTime.hour)
+		const minutes = Math.floor((resTime % intTime.hour) / intTime.minute)
+		const seconds = Math.floor((resTime % intTime.minute) / intTime.second)
+		const millis = resTime % intTime.second
+
+		moment.tz.setDefault(origTimezone)
+		return {
+			time: '' + hours + ':' + minutes + ':' + seconds + '.' + millis,
+			deadline: dLine.unix(),
+			deadlineDate: dLine.toDate(),
+		}
+	}
+}
+
+export = pdCalc
